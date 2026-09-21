35aa108d0e6a97b9c56869d4eb999c94119e6f14
bendevman <bendevman@gmail.com>
2026-09-21 20:43:37 +0300

    feat: add menu

--- src/main.js
+++ src/main.js
@@ -62,3 +62,74 @@ themeSwitch.addEventListener('click',(e)=>{
     }  
   }
 })
+
+//categories
+const tabs = body.querySelector('.tabs');
+const menu = body.querySelector('.menu');
+const menuList = body.querySelector('.menu__list');
+const tabsItems = body.querySelectorAll('.tabs__item')
+const refreshBtn = body.querySelector('.refresh-btn')
+
+const createProduct = (product) => {
+  const menuItem = document.createElement('li');
+  console.log(product)
+  menuItem.innerHTML = `
+    <div class="menu-item__pic">
+      <img src="/${product.img}" alt="coffee" class="menu-item__img">
+    </div>
+    <div class="menu-item__info">
+      <span class="menu-item__title h3">${product.name}</span>
+      <span class="menu-item__text">${product.description}</span>
+      <span class="menu-item__price h3">$${product.price}</span>
+    </div>`
+  menuItem.classList.add('menu__item','menu-item');
+  return menuItem;
+}
+const fillMenu = (products, all) => {
+  menuList.innerHTML = '';
+  let counter = 0;
+  const category = tabs.querySelector('.tabs__item_active').querySelector('.tabs__btn').dataset.category;
+  refreshBtn.classList.remove('refresh-btn_active')
+  products.forEach(product => {
+    if (category === product.category) {
+      counter++;
+      if (counter > 4 && !all) {
+        refreshBtn.classList.add('refresh-btn_active')
+        return 0;
+      }
+      menuList.appendChild(createProduct(product));
+    }
+  });
+}
+
+if (menu) {
+  if ( screen.width <= 768 ) {
+    fillMenu(products, false)
+  } else {
+    fillMenu(products, true)
+  }
+  tabs.addEventListener('click', e => {
+    const tabsItem = e.target.closest('.tabs__item')
+    if (tabsItem) {
+      tabsItems.forEach(tabsItem => {
+        tabsItem.classList.remove('tabs__item_active')
+      });
+      tabsItem.classList.add('tabs__item_active');
+      if ( screen.width <= 768 ) {
+        fillMenu(products, false)
+      } else {
+        fillMenu(products, true)
+      }
+    }
+  })
+  refreshBtn.addEventListener('click', () => {
+    fillMenu(products, true)
+  })
+  window.addEventListener("resize", () => {
+    if ( screen.width <= 768 ) {
+      fillMenu(products, false)
+    } else {
+      fillMenu(products, true)
+    }
+  });
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        about: resolve(import.meta.dirname, 'menu.html'),
+      },
+    },
     sourcemap: true,
     minify: false,
     cssMinify: false 
